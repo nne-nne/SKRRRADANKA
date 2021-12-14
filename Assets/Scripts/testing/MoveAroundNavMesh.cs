3cd680c071b4b4a62d60f41d95c9496e60b82a4b
nne-nne <adam.leczkowski1@gmail.com>
2021-12-14 15:16:39 +0100

    initial commit

---
+++ Assets/Scripts/testing/MoveAroundNavMesh.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveAroundNavMesh : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity))
+            {
+                agent.destination = hit.point;
+            }
+        }
+    }
+}
