90d22d838693cf25723fceda3d76e8d1060068aa
nne-nne <adam.leczkowski1@gmail.com>
2021-12-14 23:34:44 +0100

    funny not working

--- Assets/Scripts/Slime4Axis.cs
+++ Assets/Scripts/Slime4Axis.cs
@@ -5,27 +5,32 @@ using UnityEngine;
 public class Slime4Axis : MonoBehaviour
 {
     [SerializeField] private float rotationTime;
-    [SerializeField] private float moveForce;
-    [SerializeField] private float moveCooldown;
+    [SerializeField] private float movementTime;
+    [Tooltip("Frac 0-1")]
+
+    public Transform checkPivot;
     private Rigidbody rb;
-    private Vector2 movementDirection;
     private Vector3 lookDirection;
     private bool isRotating = false, isMoving = false;
-    
+
+    private static float tileWidth = 2f;
+    private Collider col;
+
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        col = GetComponent<Collider>();
     }
 
     void Update()
     {
-        
+
     }
 
     public void Jump(Vector2 direction)
     {
-        if(!isRotating && !isMoving)
+        if (!isRotating && !isMoving)
         {
             if (direction.x > 0)
             {
@@ -47,6 +52,8 @@ public class Slime4Axis : MonoBehaviour
             StopAllCoroutines();
             IEnumerator rotate = RotateTowards(Quaternion.Euler(lookDirection));
             StartCoroutine(rotate);
+            IEnumerator move = Move(direction);
+            StartCoroutine(move);
         }
     }
 
@@ -63,19 +70,43 @@ public class Slime4Axis : MonoBehaviour
         }
         isRotating = false;
 
-        rb.AddForce(transform.forward * moveForce, ForceMode.Impulse);
-        StartCoroutine("MoveCooldown");
+        //rb.AddForce(transform.forward * moveForce, ForceMode.Impulse);
     }
 
-    private IEnumerator MoveCooldown()
+
+
+    private IEnumerator Move(Vector2 direction)
     {
-        isMoving = true;
         float t = 0;
-        while (t < moveCooldown)
+        while (t < rotationTime)
         {
             t += Time.deltaTime;
             yield return null;
         }
-        isMoving = false;
+        col.bounds.center.Set(0f, 0.5f, 2f);
+
+
+        RaycastHit hit;
+        if (!Physics.Raycast(checkPivot.position, transform.forward, out hit, tileWidth) ||
+            hit.collider.gameObject.layer != LayerMask.NameToLayer("nonwalkable"))
+        {
+            t = 0;
+            isMoving = true;
+            Vector3 originalPosition = transform.position;
+            Vector3 targetPosition = new Vector3(originalPosition.x + direction.x * tileWidth,
+                                                     0f,
+                                                  originalPosition.z + direction.y * tileWidth);
+
+            while (t < movementTime)
+            {
+                rb.position = Vector3.Lerp(transform.position, targetPosition, t / movementTime);
+                col.bounds.center.Set(0f, 0.5f, 2-2*t/movementTime);
+                t += Time.deltaTime;
+                yield return null;
+            }
+            isMoving = false;
+            col.bounds.center.Set(0f, 0.5f, 0f);
+
+        }
     }
 }
